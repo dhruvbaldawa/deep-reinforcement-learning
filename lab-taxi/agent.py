c2d9c8dc2a21a044d9b0c351d6fbe5dee00f1eb8
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2019-09-17 16:23:15 +0400

    lab taxi

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -1,5 +1,34 @@
 import numpy as np
+import random
 from collections import defaultdict
+
+
+def epsilon_greedy(Q, state, nA, eps):
+    """Selects epsilon-greedy action for supplied state.
+
+    Params
+    ======
+        Q (dictionary): action-value function
+        state (int): current state
+        nA (int): number actions in the environment
+        eps (float): epsilon
+    """
+    if random.random() > eps: # select greedy action with probability epsilon
+        return np.argmax(Q[state])
+    else:                     # otherwise, select an action randomly
+        return random.choice(np.arange(nA))
+
+
+def update_Q_expsarsa(alpha, gamma, nA, eps, Q, state, action, reward, next_state=None):
+    """Returns updated Q-value for the most recent experience."""
+    current = Q[state][action]         # estimate in Q-table (for current state, action pair)
+    policy_s = np.ones(nA) * eps / nA  # current policy (for next state S')
+    policy_s[np.argmax(Q[next_state])] = 1 - eps + (eps / nA) # greedy action
+    Qsa_next = np.dot(Q[next_state], policy_s)         # get value of state at next time step
+    target = reward + (gamma * Qsa_next)               # construct target
+    new_value = current + (alpha * (target - current)) # get updated value
+    return new_value
+
 
 class Agent:
 
@@ -12,6 +41,7 @@ class Agent:
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.epsilon = 0.999
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -24,7 +54,7 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
+        return epsilon_greedy(self.Q, state, self.nA, self.epsilon)
 
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
@@ -37,4 +67,9 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        alpha = 1.0
+        gamma = 1.0
+        action = epsilon_greedy(self.Q, state, self.nA, self.epsilon)         # epsilon-greedy action selection
+        # update Q
+        self.Q[state][action] = update_Q_expsarsa(alpha, gamma, self.nA, self.epsilon, self.Q, state, action, reward, next_state)
+        self.epsilon = max(self.epsilon ** 3, 0.0001)
