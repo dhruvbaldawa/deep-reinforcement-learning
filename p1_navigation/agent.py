03b5c8124b41c557d25c241d73a2c62107ecec85
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2019-09-28 23:12:37 +0400

    Add model and check the agent in action

--- p1_navigation/agent.py
+++ p1_navigation/agent.py
@@ -8,17 +8,18 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 64         # minibatch size
-GAMMA = 0.99            # discount factor
-TAU = 1e-3              # for soft update of target parameters
-LR = 5e-4               # learning rate
-UPDATE_EVERY = 4        # how often to update the network
+BUFFER_SIZE = int(1e5)    # replay buffer size
+BATCH_SIZE = 64           # minibatch size
+GAMMA = 0.99              # discount factor
+TAU = 1e-3                # for soft update of target parameters
+LR = 5e-4                 # learning rate
+UPDATE_EVERY = 10          # how often to update the network
+HIDDEN_LAYERS = (64, 32)  # hidden layers for the network
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-class Agent():
+class Agent:
     """Interacts with and learns from the environment."""
 
     def __init__(self, state_size, action_size, seed):
@@ -35,9 +36,9 @@ class Agent():
         self.seed = random.seed(seed)
 
         # Q-Network
-        self.qnetwork_local = QNetwork(state_size, action_size, seed).to(device)
-        self.qnetwork_target = QNetwork(state_size, action_size, seed).to(device)
-        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
+        self.qnetwork = QNetwork(state_size, action_size, seed, hidden_layers=HIDDEN_LAYERS).to(device)
+        self.qnetwork_target = QNetwork(state_size, action_size, seed, hidden_layers=HIDDEN_LAYERS).to(device)
+        self.optimizer = optim.Adam(self.qnetwork.parameters(), lr=LR)
 
         # Replay memory
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
@@ -56,7 +57,7 @@ class Agent():
                 experiences = self.memory.sample()
                 self.learn(experiences, GAMMA)
 
-    def act(self, state, eps=0.):
+    def act(self, state, epsilon=0.):
         """Returns actions for given state as per current policy.
 
         Params
@@ -65,13 +66,13 @@ class Agent():
             eps (float): epsilon, for epsilon-greedy action selection
         """
         state = torch.from_numpy(state).float().unsqueeze(0).to(device)
-        self.qnetwork_local.eval()
+        self.qnetwork.eval()
         with torch.no_grad():
-            action_values = self.qnetwork_local(state)
-        self.qnetwork_local.train()
+            action_values = self.qnetwork(state)
+        self.qnetwork.train()
 
         # Epsilon-greedy action selection
-        if random.random() > eps:
+        if random.random() > epsilon:
             return np.argmax(action_values.cpu().data.numpy())
         else:
             return random.choice(np.arange(self.action_size))
@@ -92,7 +93,7 @@ class Agent():
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
 
         # Get expected Q values from local model
-        Q_expected = self.qnetwork_local(states).gather(1, actions)
+        Q_expected = self.qnetwork(states).gather(1, actions)
 
         # Compute loss
         loss = F.mse_loss(Q_expected, Q_targets)
@@ -102,7 +103,7 @@ class Agent():
         self.optimizer.step()
 
         # ------------------- update target network ------------------- #
-        self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)
+        self.soft_update(self.qnetwork, self.qnetwork_target, TAU)
 
     def soft_update(self, local_model, target_model, tau):
         """Soft update model parameters.
@@ -152,7 +153,7 @@ class ReplayBuffer:
         next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
         dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
 
-        return (states, actions, rewards, next_states, dones)
+        return states, actions, rewards, next_states, dones
 
     def __len__(self):
         """Return the current size of internal memory."""
--- p1_navigation/model.py
+++ p1_navigation/model.py
@@ -1,28 +1,41 @@
+import itertools
+
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
 
 class QNetwork(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=32):
+    def __init__(self, state_size, action_size, seed, hidden_layers=()):
         """Initialize parameters and build model.
         Params
         ======
             state_size (int): Dimension of each state
             action_size (int): Dimension of each action
             seed (int): Random seed
-            fc1_units (int): Number of nodes in first hidden layer
-            fc2_units (int): Number of nodes in second hidden layer
         """
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, fc1_units)
-        self.fc2 = nn.Linear(fc1_units, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, action_size)
+
+        layers = (
+            [nn.Linear(state_size, hidden_layers[0])] +
+            [(nn.Linear(start_layer, end_layer))
+                for start_layer, end_layer in zip(hidden_layers, hidden_layers[1:])
+            ] +
+            [nn.Linear(hidden_layers[-1], action_size)]
+        )
+
+        layers_with_relu = []
+        for idx, layer in enumerate(layers):
+            layers_with_relu.append(layer)
+
+            # add ReLU between all layers except the last one
+            if idx != (len(layers) - 1):
+                layers_with_relu.append(nn.ReLU())
+
+        self.layers = nn.Sequential(*layers_with_relu)
 
     def forward(self, state):
         """Build a network that maps state -> action values."""
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
+        return self.layers(state)
